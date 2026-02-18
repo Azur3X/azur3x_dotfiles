ecab19d1a3a9dac47539193bc6092d46b3946787
Bondes-xps <mathj24@student.sdu.dk>
2026-02-18 15:11:21 +0100

    Fixed clipboard issue on wayland

--- arch_files/nvim/lua/options.lua
+++ arch_files/nvim/lua/options.lua
@@ -11,4 +11,17 @@ o.shiftwidth = 4   -- Number of spaces for each indent level
 o.softtabstop = 4  -- Number of spaces for <Tab> in insert mode
 o.expandtab = true -- Convert tabs to spaces
 
+vim.g.clipboard = {
+  name = 'wl-clipboard',
+  copy = {
+    ['+'] = 'wl-copy',
+    ['*'] = 'wl-copy',
+  },
+  paste = {
+    ['+'] = 'wl-paste',
+    ['*'] = 'wl-paste',
+  },
+  cache_enabled = 0,
+}
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
