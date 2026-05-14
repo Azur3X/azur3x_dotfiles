1423de176c8de29283b995d2e7ee4236289ee40b
Bondes-xps <mathj24@student.sdu.dk>
2026-05-14 16:07:20 +0200

    Fix calender to English

--- dotfiles_wayfire/config/eww/script/calendar.sh
+++ dotfiles_wayfire/config/eww/script/calendar.sh
@@ -1,19 +1,11 @@
-#!/bin/bash
+k#!/bin/bash
 YEAR=${2:-$(date +%Y)}
 MONTH=${1:-$(date +%-m)}
-MONTH_NAME=$(date -d "$YEAR-$MONTH-01" +%B | sed '
-s/January/Januari/; s/February/Februari/; s/March/Maret/;
-s/April/April/; s/May/Mei/; s/June/Juni/;
-s/July/Juli/; s/August/Agustus/; s/September/September/;
-s/October/Oktober/; s/November/November/; s/December/Desember/')
-
-DATA=$(curl -s "https://libur.deno.dev/api?year=$YEAR&month=$MONTH")
-HOLIDAYS=$(echo "$DATA" | jq -r '.[].date')
+MONTH_NAME=$(LC_TIME=en_US.UTF-8 date -d "$YEAR-$MONTH-01" +%B)
 
 echo "<span size='large' weight='bold'>$MONTH_NAME $YEAR</span>"
 echo ""
-echo "<span weight='bold'>Sen Sel Rab Kam Jum Sab Min</span>"
-
+echo "<span weight='bold'>Mon Tue Wed Thu Fri Sat Sun</span>"
 cal -m $MONTH $YEAR | tail -n +3 | while IFS= read -r line; do
     out=""
     for (( col=0; col<7; col++ )); do
@@ -24,10 +16,8 @@ cal -m $MONTH $YEAR | tail -n +3 | while IFS= read -r line; do
             continue
         fi
         DAY_NUM=$(printf "%02d" "$day")
-        DATE="$YEAR-$(printf "%02d" "$MONTH")-$DAY_NUM"
         color="#dddddd"
         if [ $col -eq 6 ]; then color="#ff6666"; fi
-        if echo "$HOLIDAYS" | grep -q "$DATE"; then color="#ff0000"; fi
         out="$out<span foreground='$color'>$(printf "%3s" "$day")</span> "
     done
     echo "$out"
